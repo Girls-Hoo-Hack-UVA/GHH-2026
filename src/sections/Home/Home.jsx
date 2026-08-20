a4f12d8dabfc0f60bde0330383c696a117a11fd7
lissa <bmf3gm@virginia.edu>
2026-08-20 00:22:16 -0400

    added the text of the homepage

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,8 +1,28 @@
+import Hack from "./assets/hack.svg"
+import Tothe from "./assets/tothe.svg"
+import Moon from "./assets/moon.svg"
+import "../../styles/Home.css";
+
 function Home() {
   return (
     <section className="section" id="home">
-      <h1>Home page</h1>
-      <p>Placeholder</p>
+
+      <div className="home-container">
+        <div className="image-row">
+          <img src={Hack} className="hack-text" alt="Hack" />
+          <img src={Tothe} className="tothe-text" alt="to the" />
+          <img src={Moon} className="moon-text" alt="Moon" />
+        </div>
+      </div>
+
+       <div className="text-row">
+          <p className="GHH2026-text">Girls Hoo Hack 2026</p>
+            <div className="right-text">
+              <p className ="seventh-text">Girls Who Code at UVA's 7th</p>
+              <p className="annual-text">annual hackathon</p>
+            </div>
+      </div>
+      
     </section>
   )
 }
