2deadbcc1796166725f94b46717dc217d5f5a081
Lulu Wilson - lulukayw <lulu@kay-wilson.net>
2026-08-30 18:18:45 -0400

    removed home to make pr

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -6,25 +6,8 @@ import "../../styles/Home.css";
 function Home() {
   return (
     <section className="section" id="home">
-
-      <p className="date-text">October 17-18, 2026 · Charlottesville, VA</p>
-
-      <div className="home-container">
-        <div className="image-row">
-          <img src={Hack} className="hack-text" alt="Hack" />
-          <img src={Tothe} className="tothe-text" alt="to the" />
-          <img src={Moon} className="moon-text" alt="Moon" />
-        </div>
-      </div>
-
-       <div className="text-row">
-          <p className="GHH2026-text">Girls Hoo Hack 2026</p>
-            <div className="right-text">
-              <p className ="seventh-text">Girls Who Code at UVA's 7th</p>
-              <p className="annual-text">annual hackathon</p>
-            </div>
-      </div>
-      
+      <h1>Home</h1>
+      <p>Placeholder</p>
     </section>
   )
 }
