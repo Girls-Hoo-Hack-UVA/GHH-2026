4c185c95ad06a4af0b8fc61c847b910478b4d0db
Lulu Wilson - lulukayw <lulu@kay-wilson.net>
2026-09-02 10:47:55 -0400

    added holder removed unused content

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Home from './sections/Home/Home'
 import WhatIsGHH from './sections/WhatIsGHH/WhatIsGHH'
 import Schedule from './sections/Schedule/Schedule'
 import FAQ from './sections/FAQ/FAQ'
-import About from './sections/About/About'
-import Mission from './sections/Mission/Mission'
 import Statistics from './sections/Statistics/Statistics'
 import Sponsors from './sections/Sponsors/Sponsors'
 import Team from './sections/Team/Team'
@@ -11,6 +9,7 @@ import Contact from './sections/Contact/Contact'
 import Background from './sections/Background'
 import Footer from './sections/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
+import Holder from './sections/Holder/Holder'
 
 function App() {
   return (
@@ -20,14 +19,13 @@ function App() {
       <div className="content">
         <Home />
         <WhatIsGHH />
-        <Schedule />
+        {/* <Schedule /> */}
         <FAQ />
-        <About />
-        <Mission />
-        <Statistics />
-        <Sponsors />
-        <Team />
-        <Contact />
+        <Holder />
+        {/* <Statistics /> */}
+        {/* <Sponsors /> */}
+        {/* <Team /> */}
+        {/* <Contact /> */}
         <Footer />
       </div>
     </div>
---
+++ src/sections/Holder/Holder.jsx
@@ -0,0 +1,9 @@
+function Holder() {
+  return (
+    <section className="holder">
+      <h1>Sponsors and schedule coming soon</h1>
+    </section>
+  )
+}
+
+export default Holder;
